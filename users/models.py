e8c6fc77dd12260502d3b51dcf6e0e18077c4ebd
azerckid <azerckid@gmail.com>
2025-11-09 22:28:25 +0900

    Extend custom user with profile fields

--- users/models.py
+++ users/models.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """Custom user model based on Django's default implementation."""
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+        blank=True,
+        default="",
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+        blank=True,
+        default="",
+    )
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.username
