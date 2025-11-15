05d171f8e503ed8369c03227426f70ecc20ced9d
azerckid <azerckid@gmail.com>
2025-11-16 02:13:03 +0900

    Add token login route

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
-
 
 urlpatterns = [
     path("", views.Users.as_view()),
@@ -9,6 +9,7 @@ urlpatterns = [
     path("change-password", views.ChangePassword.as_view()),
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
+    path("token-login", obtain_auth_token),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
 
