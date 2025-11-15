cdbe86bb534a230d68445676e3c7d282112daddc
azerckid <azerckid@gmail.com>
2025-11-16 02:01:35 +0900

    Enable DRF token auth

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+import jwt
+
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Prefetch
 from rest_framework.response import Response
